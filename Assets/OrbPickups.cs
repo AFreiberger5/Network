83ea17c131fe9c05e5d7320092273637b08a4bb1
Adi Custovic <adi-custovic@hotmail.de>
2018-01-22 13:51:38 +0100

    -Healthorb Prefab erstellt -Switches weiter entwickelt -DMgZone Script angelegt -OrbSpawner aber Spawn funktioniert noch nicht  wirklich

---
+++ Assets/OrbPickups.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class OrbPickups : NetworkBehaviour {
+
+    void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            _col.gameObject.SetActive(false);
+            // + gib dem Spieler + x-HP
+        }
+    }
+}
---
+++ Assets/Scripts/SpecialSwitches.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpecialSwitches : NetworkBehaviour
+{
+    [SerializeField]
+    private Collider SwitchActivated;
+    // Use this for initialization
+    void Start()
+    {
+        SwitchActivated = GetComponentInChildren<Collider>();
+    }
+
+    // Update is called once per frame
+    [ServerCallback]
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider _col)
+    {
+
+        if (_col.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                SwitchActivated.enabled = !SwitchActivated.enabled;
+
+            }
+
+        }
+    }
+}
