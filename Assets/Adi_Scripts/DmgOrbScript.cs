4b7b4c12f719ed63ee8f09ee9c7eb85500902a98
Adi Custovic <adi-custovic@hotmail.de>
2018-01-26 17:38:18 +0100

    dmg continue

--- Assets/Adi_Scripts/DmgOrbScript.cs
+++ Assets/Adi_Scripts/DmgOrbScript.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class DmgOrbScript :  NetworkBehaviour
+public class DmgOrbScript : NetworkBehaviour
 {
 
     public List<string> m_collisionTags;
     private Bullet m_AddBulletDmg;
-    private int m_damage = -5;
-    private float m_damagetimer;
 
 
-    private bool DmgBoost;
 
-    void OnCollisionEnter(Collision _col)
+
+    private void OnCollisionEnter(Collider _col)
     {
         CheckCollisions(_col);
     }
-    void CheckCollisions(Collision _col)
+
+    void CheckCollisions(Collider _col)
     {
-        if (m_collisionTags.Contains(_col.collider.tag))
+        if (m_collisionTags.Contains(_col.gameObject.tag))
         {
 
             Bullet m_AddBulletDmg = _col.gameObject.GetComponent<Bullet>();
-            m_AddBulletDmg.m_damage = m_damage * 2;
             Destroy(gameObject);
-           
+
         }
     }
 }
